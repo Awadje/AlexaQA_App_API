131a19c8036aff7c8ccae2fe1ed0424eaed7bf0f
Awadje <michaelawad85@gmail.com>
2017-06-09 12:19:19 +0200

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 10.times { Traineeship.create!(topic: "Web Developer", start_date: Date.today, end_date: Date.today ) }
+10.times { OpenEvening.create!(topic: "Web Developer", date: Date.today, location: "Amsterdam" ) }
