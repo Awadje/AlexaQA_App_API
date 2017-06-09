2a5d5fa8208b91f3badbe5be8070296664c8379f
Awadje <michaelawad85@gmail.com>
2017-06-09 12:44:58 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
-10.times { Traineeship.create!(topic: "Web Developer", start_date: Date.today, end_date: Date.today ) }
-10.times { OpenEvening.create!(topic: "Web Developer", date: Date.today, location: "Amsterdam" ) }
+
+
+
+Traineeship.create!(topic: "Web Developer", start_date: Date.today - rand(50).days, end_date: Date.today - rand(50).days)
+Traineeship.create!(topic: "Web Developer: Bootcamp", start_date: Date.today - rand(50).days, end_date: Date.today - rand(50).days)
+Traineeship.create!(topic: "React Advanced", start_date: Date.today - rand(50).days, end_date: Date.today - rand(50).days)
+Traineeship.create!(topic: "Ruby Advanced", start_date: Date.today - rand(50).days, end_date: Date.today - rand(50).days)
+Traineeship.create!(topic: "Javascript Advanced", start_date: Date.today - rand(50).days, end_date: Date.today - rand(50).days)
+
+OpenEvening.create!(topic: "General Information Bootcamp", date: Date.today - rand(182).days, location: "Amsterdam" )
+OpenEvening.create!(topic: "Detailed Information Bootcamp", date: Date.today - rand(182).days, location: "Almere" )
+OpenEvening.create!(topic: "Demo Night", date: Date.today - rand(182).days, location: "Utrecht" )
+OpenEvening.create!(topic: "General Information Bootcamp", date: Date.today - rand(182).days, location: "Amsterdam" )
+OpenEvening.create!(topic: "Detailed Information Bootcamp", date: Date.today - rand(182).days, location: "Amsterdam" )
